03953d69fd75b8b8b9a5036161970c622cb70b83
Bruno <brunoalves.ds95@gmail.com>
2019-06-27 17:53:43 -0300

    Buscando por slug

--- backend/src/controllers/products-controller.js
+++ backend/src/controllers/products-controller.js
@@ -15,6 +15,18 @@ exports.get = (req, res, next) => {
     });
 }
 
+exports.getBySlug = (req, res, next) => {
+    Product.findOne(
+        { slug: req.params.slug },
+        'title description price slug tags'
+    )
+    .then(data => {
+       res.status(200).send(data) 
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+}
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product.save().
